4d02e822d648413c21dc9e0a83f6e9574ca9b25a
DyedykStanislav <dyedyk.stanislav@datawiz.io>
2021-04-26 10:25:44 +0300

    test

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.8-slim
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-RUN pip install --upgrade --no-cache-dir flake8==3.9.0 isort==5.8.0 black==20.8b1
+RUN pip install --upgrade --no-cache-dir black==20.8b1
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,11 @@ set -e
 echo "Starting Python code linters..."
 
 echo "------"
-ls
+ls /
 echo "------"
 
-echo "Running flake8..."
-flake8 $1
-echo "flake8 done!"
-
-echo "Running isort..."
-isort . --check-only --diff $2
-echo "isort done!"
-
 echo "Running black..."
-black . --check --diff $3
+black . --check --diff $1
 echo "black done!"
 
 echo "Python code linters done"
