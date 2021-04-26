bbca99d3f5fee5b76122bc3917483e9b32021e1d
DyedykStanislav <dyedyk.stanislav@datawiz.io>
2021-04-26 10:58:07 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,16 @@ set -e
 
 echo "Starting Python code linters..."
 
-echo "------"
-ls ../home
-echo "------"
+echo "Running flake8..."
+flake8 $1
+echo "flake8 done!"
+
+echo "Running isort..."
+isort . --check-only --diff $2
+echo "isort done!"
 
 echo "Running black..."
-black .
+black . --check --diff $3
 echo "black done!"
 
 echo "Python code linters done"
