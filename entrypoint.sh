fc09858b5b29977eea3c5e650b576e04e25b5a46
DyedykStanislav <dyedyk.stanislav@datawiz.io>
2021-04-27 16:28:03 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,10 +22,6 @@ ret=$?
 if ! [ $ret -eq 0 ]; then status=$ret; fi;
 echo "black done!"
 
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-echo -e "I ${RED}love${NC} Stack Overflow\n"
-
 echo "Python code linters done"
 
 exit $status
